35a8f37064531e48e4f1f3e974fcd3ae807671c4
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-10-04 22:29:00 +0800

    Made the Search Algorithm work with OnAid Database

--- Search/show.php
+++ Search/show.php
@@ -4,10 +4,10 @@
             <table  class="table table-striped" border="1" cellpadding="0"  cellspacing="0" align="center">
                 <thead>
                     <tr class="table-primary">
-                        <th width="20%">id</th>
-                        <th width="20%">title</th>
-                        <th width="50%">detail</th>
-                        <th width="10%">link</th>
+                        <th width="20%">Blog_ID</th>
+                        <th width="20%">Blog_Title</th>
+                        <th width="50%">Blog_Content</th>
+                        
                     </tr>
                 </thead>
                 <?php
@@ -16,18 +16,19 @@
                 echo $_GET['q'];
                 echo '</font>';
                 echo '<br/>';             
-                $sql = "SELECT * FROM tbl_search
-                    WHERE title LIKE '%$q%' OR detail LIKE '%$q%'
-                 ORDER BY id DESC";
+                $sql = "SELECT * FROM blogs
+                    WHERE blog_title LIKE '%$q%' OR blog_content LIKE '%$q%'
+                 ORDER BY blog_id DESC";
                 $result = mysqli_query($con, $sql);
-                while($row = mysqli_fetch_array($result)) {
+                while($row = mysqli_fetch_assoc($result)) {
                                     
                 ?>
+                
                 <tr>
-                    <td><?php echo $row['id'];?></td>
-                    <td><?php echo $row['title'];?></td>
-                    <td><?php echo $row['detail'];?></td>
-                    <td><?php echo $row['link'];?></td>
+                    <td><?php echo $row['blog_id'];?></td>
+                    <td><?php echo $row['blog_title'];?></td>
+                    <td><?php echo $row['blog_content'];?></td>
+                    
                 </tr>
             <?php } ?>
             </table>
