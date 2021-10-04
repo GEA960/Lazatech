38234643260d88d3afdbf7852c3b4b15a3c26484
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-10-04 20:57:23 +0800

    Added a Search Algorithm

---
+++ Search/footer.php
@@ -0,0 +1,6 @@
+<p align="center" style="margin-top: 200px"> OnAid Search 2021
+	<br>
+	Integrated by OnAid.org
+	<br>
+	สนับสนุน devbanban.com
+	<a href="http://devbanban.com/?page_id=2309"> Click here to return to Dashboard </a></p>
---
+++ Search/navbar.php
@@ -0,0 +1,31 @@
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">OnAid Search</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="https://www.youtube.com/playlist?list=PLEA4F1w-xYVbvUVUgN3WnJ35qfTH3KktB&disable_polymer=true" target="_blank">Dashboard</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="https://www.youtube.com/playlist?list=PLEA4F1w-xYVa0P11woJOAPty8DSLemepR&disable_polymer=true" target="_blank">Messages</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="https://www.youtube.com/watch?v=PsxN5lSEXfo&t=1s&index=2&list=PLEA4F1w-xYVZX-h5l0xYldQCvZJR4kvft" target="_blank">People</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="http://devbanban.com/" target="_blank">OnAid.org</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</div>
