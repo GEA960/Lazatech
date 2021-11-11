a3731676fa5a1a0881c4b61d4b249ab986bad20c
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-11-11 23:21:34 +0800

    rename the cover picture of all users

--- includes/profile-card.php
+++ includes/profile-card.php
@@ -1,7 +1,7 @@
 
 
             <div class='card card-profile text-center'>
-                <img alt='' class='card-img-top card-user-cover' src='/img/user_cover.png'>
+                <img alt='' class='card-img-top card-user-cover' src='img/User_Cover.png'>
                 <div class='card-block'>
                     <a href='profile.php'>
                         <img src='uploads/<?php echo $_SESSION["userImg"] ?>' class='card-img-profile'>
