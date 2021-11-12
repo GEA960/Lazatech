1730e3fbb3c521c00eeb321a6a2d421024fe2f02
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-11-12 18:55:32 +0800

    Fixed the phpmyadmin account pass

--- 1condb.php
+++ 1condb.php
@@ -2,6 +2,6 @@
 $con= mysqli_connect("localhost","root","adqUKjS5Tg31JoZH","onaid_database") or die("Error: " . mysqli_error($con));
 mysqli_query($con, "SET NAMES 'utf8' ");
 date_default_timezone_set('Asia/Manila');
-//OnAid.com
+//OnAidOrg.com
 
 ?>
