1d681d0775a3602922d3b772901c27f659b7e82e
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-11-16 15:17:25 +0800

    added radio buttons on index and created functions for search on 4 tables

--- 1form.php
+++ 1form.php
@@ -9,9 +9,25 @@
 					<div class="col-sm-5">
 						<input type="text" name="q" class="form-control"  required>
 					</div>
+					<p>Please select here what values to search to:</p>
+   				<input type="radio" id="forum" name="pref_value" value="forum">
+  				<label for="forum">Polls</label><br>
+  	 			<input type="radio" id="blog" name="pref_value" value="blog">
+  				<label for="blog">Blogs</label><br>
+  				<input type="radio" id="poll" name="pref_value" value="poll">
+  				<label for="poll">Polls</label>
+  				<input type="radio" id="events" name="pref_value" value="event">
+  				<label for="event">Events</label>  
+
 					<div class="col-sm-2">
-						<button type="submit" class="btn btn-primary">Submit
+					<button type="submit" class="btn btn-primary">Submit
 						</button>
+						
+  
+  
+ 
+
+
 					</div>
 				</div>
 			</form>
--- 1show.php
+++ 1show.php
@@ -1,3 +1,7 @@
+
+
+
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -5,7 +9,54 @@
                 
                 <thead>
                     <tr class="table-primary">
-                        <th width="20%">Blog_ID</th>
+                        <th width="50%">Forum</th>
+                        <th width="30%">Link</th>
+                       
+                        
+                    </tr>
+                </thead>
+                <?php    
+                echo '<font color="red">';   
+                echo 'Keyword(s) = ';
+                echo $_GET['q'];
+                echo '</font>';
+                echo '<br/>';             
+                $sql = "SELECT * FROM posts
+                    WHERE post_content LIKE '%$q%' 
+                 ORDER BY post_votes DESC";
+                $result = mysqli_query($con, $sql);
+                while($row = mysqli_fetch_assoc($result)) {
+                                    
+                ?>
+                
+                <tr>
+                    <td><?php echo $row['post_content'];?></td> 
+                </tr>
+            <?php } ?>
+            </table>
+        <?php mysqli_close($con);     ?>    
+    </div>
+</div>
+</div>
+
+             
+
+
+
+
+
+
+
+
+<!--
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <table  class="table table-striped" border="1" cellpadding="0"  cellspacing="0" align="center">
+                
+                <thead>
+                    <tr class="table-primary">
                         <th width="20%">Blog_Title</th>
                         <th width="50%">Blog_Content</th>
                         <th width="30%">Link</th>
@@ -13,7 +64,7 @@
                         
                     </tr>
                 </thead>
-                <?php
+                <?php /*
                 echo '<font color="red">';   
                 echo 'Keyword(s) = ';
                 echo $_GET['q'];
@@ -28,14 +79,108 @@
                 ?>
                 
                 <tr>
-                    <td><?php echo $row['blog_id'];?> </td>
                     <td><?php echo $row['blog_title'];?></td>
                     <td><?php echo $row['blog_content'];?></td>
                     <td>   </td> 
                 </tr>
             <?php } ?>
             </table>
-        <?php mysqli_close($con);?>
+        <?php mysqli_close($con);   */?>
     </div>
 </div>
 </div>
+
+                -->
+
+
+
+<!--
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <table  class="table table-striped" border="1" cellpadding="0"  cellspacing="0" align="center">
+                
+                <thead>
+                    <tr class="table-primary">
+                        <th width="20%">Poll Subject</th>
+                        <th width="50%">Poll Description</th>
+                        <th width="30%">Link</th>
+                       
+                        
+                    </tr>
+                </thead>
+                <?php     /*
+                echo '<font color="red">';   
+                echo 'Keyword(s) = ';
+                echo $_GET['q'];
+                echo '</font>';
+                echo '<br/>';             
+                $sql = "SELECT * FROM polls
+                    WHERE 'subject' LIKE '%$q%' OR poll_desc LIKE '%$q%'
+                 ORDER BY id DESC";
+                $result = mysqli_query($con, $sql);
+                while($row = mysqli_fetch_assoc($result)) {
+                                    
+                ?>
+                
+                <tr>
+                    <td><?php echo $row['subject'];?></td>
+                    <td><?php echo $row['poll_desc'];?></td>
+                    <td>   </td> 
+                </tr>
+            <?php } ?>
+            </table>
+        <?php mysqli_close($con);  */  ?>
+    </div>
+</div>
+</div>               
+
+                -->
+
+
+
+
+<!--
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <table  class="table table-striped" border="1" cellpadding="0"  cellspacing="0" align="center">
+                
+                <thead>
+                    <tr class="table-primary">
+                        <th width="20%">Event Title</th>
+                        <th width="50%">Event Headline</th>
+                        <th width="30%">Event Description</th>
+                        <th width="30%">Link</th>
+                       
+                        
+                    </tr>
+                </thead>
+                <?php     /*
+                echo '<font color="red">';   
+                echo 'Keyword(s) = ';
+                echo $_GET['q'];
+                echo '</font>';
+                echo '<br/>';             
+                $sql = "SELECT * FROM event_info
+                    WHERE title LIKE '%$q%' OR headline LIKE '%$q%' OR 'description' LIKE '%$q%'
+                 ORDER BY event_id DESC";
+                $result = mysqli_query($con, $sql);
+                while($row = mysqli_fetch_assoc($result)) {
+                                    
+                ?>
+                
+                <tr>
+                    <td><?php echo $row['title'];?></td>
+                    <td><?php echo $row['headline'];?></td>
+                    <td><?php echo $row['headline'];?></td>
+                    <td>   </td> 
+                </tr>
+            <?php } ?>
+            </table>
+        <?php mysqli_close($con); */ ?>
+    </div>
+</div>
+</div>               
+
+                -->
