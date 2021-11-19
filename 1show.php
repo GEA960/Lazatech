57f4e704e37c81613e2c3a6897e99bca8f523348
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-11-19 15:06:54 +0800

    Added a substr function for the displaying of blog content

--- 1show.php
+++ 1show.php
@@ -85,7 +85,7 @@ if($_GET['pref_value'] == 'blog'){
                 
                 echo '<tr>';
             echo '<td>'; echo $row['blog_title']; echo '</td>'; 
-            echo '<td>'; echo $row['blog_content']; echo '</td>'; 
+            echo '<td>'; echo substr($row['blog_content'],0,100); echo '...</td>'; 
             echo '<td> <a href= "http://onaid/blog-page.php?id=';  echo $row['blog_id']; echo'"><Center> <Button>LINK</Button> </Center></a> </td>'; 
             echo '</tr>';
              } 
@@ -101,101 +101,7 @@ if($_GET['pref_value'] == 'blog'){
 
 
 
-             
 
-if($_GET['pref_value'] == 'blog'){
-
-    echo '
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <table  class="table table-striped" border="1" cellpadding="0"  cellspacing="0" align="center">
-                
-                <thead>
-                    <tr class="table-primary">
-                    <th width="20%">Blog_Title</th>
-                    <th width="50%">Blog_Content</th>
-                    <th width="30%">Link</th>
-                       
-                        
-                    </tr>
-                </thead>';
-                
-                echo '<font color="red">';   
-                echo 'Keyword(s) = ';
-                echo $_GET['q'];
-                echo '</font>';
-                echo '<br/>';             
-                $sql = "SELECT * FROM blogs
-                WHERE blog_title LIKE '%$q%' OR blog_content LIKE '%$q%'
-                ORDER BY blog_id DESC";
-                $result = mysqli_query($con, $sql);
-                while($row = mysqli_fetch_assoc($result)) {
-                                    
-                
-                
-                echo '<tr>';
-            echo '<td>'; echo $row['blog_title']; echo '</td>'; 
-            echo '<td>'; echo $row['blog_content']; echo '</td>'; 
-            echo '<td> <a href= "http://onaid/blog-page.php?id=';  echo $row['blog_id']; echo'"><Center> <Button>LINK</Button> </Center></a> </td>'; 
-            echo '</tr>';
-             } 
-             echo ' </table>';
-        mysqli_close($con);  
-        echo '</div>
-</div>
-</div>';
-
-             
-             } if($_GET['pref_value'] == 'blog'){
-
-    echo '
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <table  class="table table-striped" border="1" cellpadding="0"  cellspacing="0" align="center">
-                
-                <thead>
-                    <tr class="table-primary">
-                    <th width="20%">Blog_Title</th>
-                    <th width="50%">Blog_Content</th>
-                    <th width="30%">Link</th>
-                       
-                        
-                    </tr>
-                </thead>';
-                
-                echo '<font color="red">';   
-                echo 'Keyword(s) = ';
-                echo $_GET['q'];
-                echo '</font>';
-                echo '<br/>';             
-                $sql = "SELECT * FROM blogs
-                WHERE blog_title LIKE '%$q%' OR blog_content LIKE '%$q%'
-                ORDER BY blog_id DESC";
-                $result = mysqli_query($con, $sql);
-                while($row = mysqli_fetch_assoc($result)) {
-                                    
-                
-                
-                echo '<tr>';
-            echo '<td>'; echo $row['blog_title']; echo '</td>'; 
-            echo '<td>'; echo $row['blog_content']; echo '</td>'; 
-            echo '<td> <a href= "http://onaid/blog-page.php?id=';  echo $row['blog_id']; echo'"><Center> <Button>LINK</Button> </Center></a> </td>'; 
-            echo '</tr>';
-             } 
-             echo ' </table>';
-        mysqli_close($con);  
-        echo '</div>
-</div>
-</div>';
-
-             
-             }              
-             
-             
-             
-             
 
              
 
