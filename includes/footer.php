8c1a6d24031708a2ae8f036497ac674700248db8
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-11-20 10:26:00 +0800

    Added Paypal Donate Button

--- includes/footer.php
+++ includes/footer.php
@@ -19,16 +19,33 @@
                     <h5>About us</h5>
                     <ul>
                         <li><a href="https://gea960.github.io/GEA-Portfolio-Website/">Gabriel Edrian Alvaro</a></li>
-                        <li><a href="OnAid_Creators/          .php">Lee Ann Joy Natanauan</a></li>
-                        <li><a href="OnAid_Creators/            .php">Maezie Landicho Casabal</a></li>
-                        <li><a href="OnAid_Creators/            .php">Marjori Javier</a></li>
-                        <li><a href="OnAid_Creators/            .php">Jhonrey Manalo</a></li>
+                        <li><a href="includes/OnAid_Creators/Lee-ann.php">Lee Ann Joy Natanauan</a></li>
+                        <li><a href="includes/OnAid_Creators/Maezie.php">Maezie Landicho Casabal</a></li>
+                        <li><a href="includes/OnAid_Creators/Marjorie.php">Marjori Javier</a></li>
+                        <li><a href="includes/OnAid_Creators/Johnrey.php">Jhonrey Manalo</a></li>
                     </ul>
                 </div>
                 <div>
                     <h5>Support</h5>
                     <ul>
+                        
                         <li><a href="contact.php">Contact Us</a></li>
+                        <li> <center><div id="donate-button-container">
+                            <div id="donate-button"></div>
+                            <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script>
+                            <script>
+                            PayPal.Donation.Button({
+                            env:'production',
+                            hosted_button_id:'JGHAEUUY6BWHQ',
+                            image: {
+                            src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif',
+                            alt:'Donate with PayPal button',
+                            title:'PayPal - The safer, easier way to pay online!',
+                            }
+                            }).render('#donate-button');
+                            </script>
+                            </div> </center></li>
+
                     </ul>
                 </div>
              
