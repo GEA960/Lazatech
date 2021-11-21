21952c50407ce18657f11b7a9a42c8c22ded3914
Gabriel Edrian Alvaro <gabrielalvaro693@gmail.com>
2021-11-21 15:33:16 +0800

    Fixed the title to OnAid

--- topics.php
+++ topics.php
@@ -49,7 +49,7 @@
       <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded shadow-sm">
           <img class="mr-3" src="img/200px.png" alt="" width="48" height="48">
         <div class="lh-100">
-          <h1 class="mb-0 text-white lh-100">KLiK Forums</h1>
+          <h1 class="mb-0 text-white lh-100">OnAid Forums</h1>
           <small>Spreading Ideas</small>
         </div>
       </div>  
