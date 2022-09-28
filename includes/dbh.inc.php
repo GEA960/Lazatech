fc404bb4e13b57e8e434f1a5121af117df4896ab
Gabriel Edrian A. Alvaro <75560222+GEA960@users.noreply.github.com>
2022-09-28 15:22:58 +0800

    Update db connection with new hostinger config

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "adqUKjS5Tg31JoZH";
-$dBName = "lazatech_database";
+$dBUsername = "u488180748_LAZAT3CH";
+$dBPassword = "LAZAT3CHPass";
+$dBName = "u488180748_LAZAT3CH";
 
 $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName, 3306);
 
