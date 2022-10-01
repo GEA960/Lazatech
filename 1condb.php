4684dedffe5832c1701d38c4b46948eecc0b259b
Gabriel Edrian A. Alvaro <75560222+GEA960@users.noreply.github.com>
2022-10-01 23:02:56 +0800

    Updated the database connection

--- 1condb.php
+++ 1condb.php
@@ -1,5 +1,5 @@
 <?php
-$con= mysqli_connect("localhost","root","adqUKjS5Tg31JoZH","lazatech_database") or die("Error: " . mysqli_error($con));
+$con= mysqli_connect("localhost","u488180748_LAZAT3CH","LAZAT3CHPass","u488180748_LAZATECH") or die("Error: " . mysqli_error($con));
 
 mysqli_query($con, "SET NAMES 'utf8' ");
 date_default_timezone_set('Asia/Manila');
