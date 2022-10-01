d416ffa1059b5da2a97deb308ff533d7d516ce3b
Gabriel Edrian A. Alvaro <75560222+GEA960@users.noreply.github.com>
2022-10-01 23:00:56 +0800

    Added 1condb.php

---
+++ 1condb.php
@@ -0,0 +1,7 @@
+<?php
+$con= mysqli_connect("localhost","root","adqUKjS5Tg31JoZH","lazatech_database") or die("Error: " . mysqli_error($con));
+
+mysqli_query($con, "SET NAMES 'utf8' ");
+date_default_timezone_set('Asia/Manila');
+//OnAidOrg.com
+?>
