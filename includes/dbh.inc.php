a2a7a87b78d856bcd119459dfb1bae0e913b0a38
Gabriel Edrian A. Alvaro <75560222+GEA960@users.noreply.github.com>
2022-10-01 11:32:33 +0800

    Corrected the db name

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dBUsername = "u488180748_LAZAT3CH";
 $dBPassword = "LAZAT3CHPass";
-$dBName = "u488180748_LAZAT3CH";
+$dBName = "u488180748_LAZATECH";
 
 $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName, 3306);
 
