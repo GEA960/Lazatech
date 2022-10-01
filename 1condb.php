a228929609f68e8a8ec6f1c1ae6e32ee8cd38f9d
Gabriel19-00477 <gabrieledrian.alvaro@g.batstate-u.edu.ph>
2022-10-01 22:50:08 +0800

    .gitignore is now working

--- 1condb.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con= mysqli_connect("localhost","u488180748_LAZAT3CH","LAZAT3CHPass","u488180748_LAZATECH") or die("Error: " . mysqli_error($con));
-
-mysqli_query($con, "SET NAMES 'utf8' ");
-date_default_timezone_set('Asia/Manila');
-//OnAidOrg.com
-?>
