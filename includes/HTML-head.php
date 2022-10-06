06139ef8b64661d9a002c2e065a0c6f7f1e55256
Gabriel Edrian A. Alvaro <98994784+Gabriel19-00477@users.noreply.github.com>
2022-10-06 16:11:50 +0800

    Updated the style.css

--- includes/HTML-head.php
+++ includes/HTML-head.php
@@ -10,7 +10,7 @@
         <link href="css/bootstrap.min.css" rel="stylesheet"> 
         <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"> 
         <link href="css/footer.css" rel="stylesheet">
-        <link href="css/styles.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
     
         
         
