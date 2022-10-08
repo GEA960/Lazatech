c1505c4eaf05b5cf67213ead881f02a9a61b7427
GuerraMarielZ <guerramarielz09@gmail.com>
2022-10-08 10:09:38 +0800

    ...

--- index.php
+++ index.php
@@ -55,21 +55,13 @@
 
                     <div class="col-sm-2">
 
-                        <div class="text-center p-3 mt-5">
-                        <!--    <a href="team.php" target="_blank">
-                                <i class="creater-icon fa fa-users fa-5x" aria-hidden="true"></i>
-                            </a>
-                            <p><br>THE CREATORS</p> -->
-                            <a> </a>
-                            <p><br></br><br></br><br></br><br></br><br></br></p>
-                        </div>
-
-                        <a href="forum.php" class="btn btn-warning btn-lg btn-block">OnAid Forum</a>
-                        <a href="hub.php" class="btn btn-secondary btn-lg btn-block">OnAid Hub</a>
-                        <br><br><br>
+                        
                         <a href="create-topic.php" class="btn btn-warning btn-lg btn-block">Create a Forum</a>
                         <a href="create-blog.php" class="btn btn-secondary btn-lg btn-block">Create a Blog</a>
                         <a href="create-poll.php" class="btn btn-warning btn-lg btn-block">Create a Poll</a>
+                        <a href="forum.php" class="btn btn-warning btn-lg btn-block">OnAid Forum</a>
+                        <a href="hub.php" class="btn btn-secondary btn-lg btn-block">OnAid Hub</a>
+                        
 
                     </div>
                     <div class="col-sm-9" >
