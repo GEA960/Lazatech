1fda92079ee42adab422a5e78d4beb3f5a03e565
GuerraMarielZ <guerramarielz09@gmail.com>
2022-10-08 10:32:43 +0800

    ...

--- forum.php
+++ forum.php
@@ -24,7 +24,7 @@
    
 
         <main role="main" class="container">
-      <div class="d-flex align-items-center p-3 my-3 text-white-50 rounded shadow-sm">
+      <div class="d-flex align-items-center p-3 my-3 bg-info text-white-50 rounded shadow-sm">
           <img class="mr-3" src="img/logo.png" alt="" width="48" height="48">
         <div class="lh-100">
           <h1 class="mb-0 text-white lh-100">Lazatech Forums</h1>
