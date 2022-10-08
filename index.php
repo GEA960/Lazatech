6d244e71cd7c24b77b240df08c81848db9f8f2c6
GuerraMarielZ <guerramarielz09@gmail.com>
2022-10-08 09:58:23 +0800

    update index.php

--- index.php
+++ index.php
@@ -46,21 +46,40 @@
             <?php include 'includes/navbar.php'; ?> 
             
             <div class="container-fluid">
-                <div class="row">
-                <div class="col-sm-3" >
+                <div class="row justify-content-center">
+                <div class="col-sm-11" >
 
                         <?php include 'includes/profile-card.php'; ?>
 
                     </div>
 
-                    <div class="col-sm-7" >
+                    <div class="col-sm-2">
 
+                        <div class="text-center p-3 mt-5">
+                        <!--    <a href="team.php" target="_blank">
+                                <i class="creater-icon fa fa-users fa-5x" aria-hidden="true"></i>
+                            </a>
+                            <p><br>THE CREATORS</p> -->
+                            <a> </a>
+                            <p><br></br><br></br><br></br><br></br><br></br></p>
+                        </div>
+
+                        <a href="forum.php" class="btn btn-warning btn-lg btn-block">OnAid Forum</a>
+                        <a href="hub.php" class="btn btn-secondary btn-lg btn-block">OnAid Hub</a>
+                        <br><br><br>
+                        <a href="create-topic.php" class="btn btn-warning btn-lg btn-block">Create a Forum</a>
+                        <a href="create-blog.php" class="btn btn-secondary btn-lg btn-block">Create a Blog</a>
+                        <a href="create-poll.php" class="btn btn-warning btn-lg btn-block">Create a Poll</a>
+
+                    </div>
+                    <div class="col-sm-9" >
+<!--
                         <div class="text-center p-3">
                             <img src="img/200pxFinal.png">
                             <h2 class='text-muted'>DASHBOARD</h2>
                             <br>
                         </div>
-
+-->
                           
                         <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
                             <li class="nav-item">
@@ -94,7 +113,7 @@
                                   </div>
                                 </div>  
 
-                                    <div class="row mb-2">
+                                    <div class="mb-2">
 
                                     <?php
                                         $sql = "select topic_id, topic_subject, topic_date, topic_cat, topic_by, userImg, idUsers, uidUsers, cat_name, (
@@ -121,7 +140,7 @@
 
                                             while ($row = mysqli_fetch_assoc($result))
                                             {
-                                                echo '<div class="col-md-6">
+                                                echo '<div class="col-md-14">
                                                         <div class="card flex-md-row mb-4 shadow-sm h-md-250">
                                                         <a href="posts.php?topic='.$row['topic_id'].'">
                                                         <img class="card-img-left flex-auto d-none d-lg-block blogindex-cover" 
@@ -160,7 +179,7 @@
                                   </div>
                                 </div>  
 
-                                <div class="row mb-2">
+                                <div class="mb-2">
 
                                     <?php
                                         $sql = "select * from blogs, users 
@@ -184,6 +203,10 @@
                                                 echo '<div class="col-md-6">
                                                         <div class="card flex-md-row mb-4 shadow-sm h-md-250">
                                                           <div class="card-body d-flex flex-column align-items-start">
+                                                          <a href="blog-page.php?id='.$row['blog_id'].'">
+                                                          <img class="card-img-right flex-auto d-none d-lg-block blogindex-cover" 
+                                                                src="uploads/'.$row['blog_img'].'" alt="Card image cap">
+                                                                    </a>
                                                             <strong class="d-inline-block mb-2 text-primary">
                                                                 <i class="fa fa-thumbs-up" aria-hidden="true"></i> '.$row['blog_votes'].'
                                                             </strong>
@@ -194,10 +217,7 @@
                                                             <small class="card-text mb-auto">'.substr($row['blog_title'],0,100).'...</small>
                                                             <a href="blog-page.php?id='.$row['blog_id'].'">Continue reading</a>
                                                           </div>
-                                                          <a href="blog-page.php?id='.$row['blog_id'].'">
-                                                          <img class="card-img-right flex-auto d-none d-lg-block blogindex-cover" 
-                                                                src="uploads/'.$row['blog_img'].'" alt="Card image cap">
-                                                                    </a>
+                                                          
                                                         </div>
                                                       </div>';
                                             }
@@ -339,25 +359,6 @@
 
                     </div>
 
-                    <div class="col-sm-2">
-
-                        <div class="text-center p-3 mt-5">
-                        <!--    <a href="team.php" target="_blank">
-                                <i class="creater-icon fa fa-users fa-5x" aria-hidden="true"></i>
-                            </a>
-                            <p><br>THE CREATORS</p> -->
-                            <a> </a>
-                            <p><br></br><br></br><br></br><br></br><br></br></p>
-                        </div>
-
-                        <a href="forum.php" class="btn btn-warning btn-lg btn-block">OnAid Forum</a>
-                        <a href="hub.php" class="btn btn-secondary btn-lg btn-block">OnAid Hub</a>
-                        <br><br><br>
-                        <a href="create-topic.php" class="btn btn-warning btn-lg btn-block">Create a Forum</a>
-                        <a href="create-blog.php" class="btn btn-secondary btn-lg btn-block">Create a Blog</a>
-                        <a href="create-poll.php" class="btn btn-warning btn-lg btn-block">Create a Poll</a>
-
-                    </div>
                 </div>
             </div>
             
