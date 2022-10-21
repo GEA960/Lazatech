be7fd122b47c65d8aba80e64fde9952a2f23e0a9
Gabby <gabrielalvaro693@gmail.com>
2022-10-21 19:23:50 +0800

    add a condition to let an admin delete an event

--- event-page.php
+++ event-page.php
@@ -138,7 +138,7 @@
                         </div>
                         <?php
 
-                            if ($_SESSION['userLevel'] == 1 || $_SESSION['userId'] == $row['event_by'])
+                            if ($_SESSION['userLevel'] == 1 || $_SESSION['userId'] == $row['event_by'] || ($_SESSION['userLevel'] == 2))
                             {
                                 echo '<a href="includes/delete-event.php?id='.$row['event_id'].'&page=events" >
                                         <i class="fa fa-trash" aria-hidden="true" style="color: red;"></i>
