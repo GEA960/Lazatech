25d6e87ae021593e0e33383c65b5922d9dabb074
GuerraMarielZ <guerramarielz09@gmail.com>
2022-10-22 11:27:33 +0800

    update topics.php

--- topics.php
+++ topics.php
@@ -47,9 +47,9 @@
 
         <main role="main" class="container">
       <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded shadow-sm">
-          <img class="mr-3" src="img/200px.png" alt="" width="48" height="48">
+          <img class="mr-3" src="img/logo.png" alt="" width="48" height="48">
         <div class="lh-100">
-          <h1 class="mb-0 text-white lh-100">OnAid Forums</h1>
+          <h1 class="mb-0 text-white lh-100">Lazatech Forums</h1>
           <small>Spreading Ideas</small>
         </div>
       </div>  
