979f5a15929b4ae05fda904aa96789d9d9477311
GuerraMarielZ <guerramarielz09@gmail.com>
2022-10-26 22:19:20 +0800

    sign up email verification

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -66,30 +66,41 @@ if (isset($_POST['signup-submit']))
             }
             else
             {
-                $FileNameNew = 'default.png';
-                require 'upload.inc.php';
-                
-                $sql = "insert into users(uidUsers, emailUsers, f_name, l_name, pwdUsers, gender, "
-                        . "headline, bio, userImg) "
-                        . "values (?,?,?,?,?,?,?,?,?)";
-                $stmt = mysqli_stmt_init($conn);
-                if (!mysqli_stmt_prepare($stmt, $sql))
+                $sql1 = "select * from users where emailUsers = '$email'";
+                $res = mysqli_query($conn, $sql1);
+
+                if(mysqli_num_rows($res) > 0)
                 {
-                    header("Location: ../signup.php?error=sqlerror");
-                    exit();
+                    header("Location: ../signup.php?error=emailexist&mail=".$email);
+                    exit();  
                 }
                 else
                 {
-                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+                    $FileNameNew = 'default.png';
+                    require 'upload.inc.php';
                     
-                    mysqli_stmt_bind_param($stmt, "sssssssss", $userName, $email, $f_name, $l_name,
-                            $hashedPwd, $gender,
-                            $headline, $bio, $FileNameNew);
-                    mysqli_stmt_execute($stmt);
-                    mysqli_stmt_store_result($stmt);
-                    
-                    header("Location: ../signup.php?signup=success");
-                    exit();
+                    $sql = "insert into users(uidUsers, emailUsers, f_name, l_name, pwdUsers, gender, "
+                            . "headline, bio, userImg) "
+                            . "values (?,?,?,?,?,?,?,?,?)";
+                    $stmt = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmt, $sql))
+                    {
+                        header("Location: ../signup.php?error=sqlerror");
+                        exit();
+                    }
+                    else
+                    {
+                        $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+                        
+                        mysqli_stmt_bind_param($stmt, "sssssssss", $userName, $email, $f_name, $l_name,
+                                $hashedPwd, $gender,
+                                $headline, $bio, $FileNameNew);
+                        mysqli_stmt_execute($stmt);
+                        mysqli_stmt_store_result($stmt);
+                        
+                        header("Location: ../signup.php?signup=success");
+                        exit();
+                    }
                 }
             }
         }
