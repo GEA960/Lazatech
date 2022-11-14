15ababffdf90efa50754f1a39f1167800cc068d6
Gabby <gabrielalvaro693@gmail.com>
2022-11-14 09:28:42 +0800

    Added Mailing pass in db and removing it to script for security purposes

--- includes/email-server.php
+++ includes/email-server.php
@@ -1,7 +1,23 @@
 <?php
+ include_once 'includes/dbh.inc.php';
+
+ $sql = "select * from users where idUsers = 61";
+    $stmt = mysqli_stmt_init($conn);    
+    
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        die('SQL error');
+    }
+    else
+    {
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        $user = mysqli_fetch_assoc($result);
+    }
+    
 
 $SMTPuser = 'lazatech.org@gmail.com';   
-$SMTPpwd = 'begvgstgelmmnpyp'; 
+$SMTPpwd = $user['MailPass']; 
 $SMTPtitle = "Lazatech inc.";
 $Domain = 'localhost';
 
