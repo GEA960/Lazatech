2b76ff6b9aca9775756bed891f4859eb964e12b8
Gabby <gabrielalvaro693@gmail.com>
2022-11-15 20:06:39 +0800

    Event head, event title, and event description are now all free from single quote error

--- Event-Insert.php
+++ Event-Insert.php
@@ -6,8 +6,12 @@ require 'includes/dbh.inc.php';
 if(isset($_POST['update']))
 {
     $id = $_POST['event_id'];
+    
+    $EventTitle = str_replace("'", "''", $_POST['title']);
+    $EventHeadline = str_replace("'", "''", $_POST['headline']);
     $EventDescription = str_replace("'", "''", $_POST['description']);
-    $query = "UPDATE events, event_info SET events.title='$_POST[title]', events.event_date = '$_POST[edate]', event_info.title='$_POST[title]', event_info.headline='$_POST[headline]', event_info.description='$EventDescription' WHERE events.event_id='$_POST[event_id]' AND event_info.event='$_POST[event_id]'";
+    
+    $query = "UPDATE events, event_info SET events.title='$EventTitle', events.event_date = '$_POST[edate]', event_info.title='$EventTitle', event_info.headline='$EventHeadline', event_info.description='$EventDescription' WHERE events.event_id='$_POST[event_id]' AND event_info.event='$_POST[event_id]'";
    
 
     $query_run = mysqli_query($conn, $query);                                                                  
