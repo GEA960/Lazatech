7391a59a111b08aef5dbc86ec6de716fc03d85a6
Gabby <gabrielalvaro693@gmail.com>
2022-11-15 19:30:12 +0800

    Error in single quotes in content now fix for event description

--- Event-Insert.php
+++ Event-Insert.php
@@ -1,15 +1,13 @@
 <?php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 require 'includes/dbh.inc.php';
 
 if(isset($_POST['update']))
 {
     $id = $_POST['event_id'];
-    $query = "UPDATE events, event_info SET events.title='$_POST[title]', events.event_date = '$_POST[edate]', event_info.title='$_POST[title]', event_info.headline='$_POST[headline]', event_info.description='$_POST[description]' WHERE events.event_id='$_POST[event_id]' AND event_info.event='$_POST[event_id]'";
+    $EventDescription = str_replace("'", "''", $_POST['description']);
+    $query = "UPDATE events, event_info SET events.title='$_POST[title]', events.event_date = '$_POST[edate]', event_info.title='$_POST[title]', event_info.headline='$_POST[headline]', event_info.description='$EventDescription' WHERE events.event_id='$_POST[event_id]' AND event_info.event='$_POST[event_id]'";
    
 
     $query_run = mysqli_query($conn, $query);                                                                  
