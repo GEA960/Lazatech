811a15eea218b805eb6b2c7be75dbb3023ee1646
Gabby <gabrielalvaro693@gmail.com>
2022-12-07 12:03:19 +0800

    Reply forum will not accept plagiarized content with >50 percentage of plag

--- posts.php
+++ posts.php
@@ -52,6 +52,66 @@
     if (isset($_POST['submit-reply']))
     {
         $content = $_POST['reply-content'];
+
+        $sql = "select * from users where idUsers = 61";
+        $stmt = mysqli_stmt_init($conn);    
+        
+        if (!mysqli_stmt_prepare($stmt, $sql))
+        {
+            die('SQL error');
+        }
+        else
+        {
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            $user = mysqli_fetch_assoc($result);
+
+            $txt = $content;
+            
+            $postData = [ "text" => $txt,
+                        "language" => "en",
+                        "includeCitations" => "false",
+                        "scrapeSources" => "false"
+                        ];
+            $api =  $user['PlagAPIKey'];
+
+            $curl = curl_init();
+            
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com/plagiarism",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => json_encode($postData),
+                CURLOPT_HTTPHEADER => [
+     "X-RapidAPI-Host: plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com",
+     "X-RapidAPI-Key: $api",
+     "content-type: application/json"
+                ],
+            ]);
+            
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            
+            curl_close($curl);
+         
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                $response = json_decode($response, TRUE);
+                
+                if ($response['percentPlagiarism'] >= "50" ) {
+                    
+                    echo '<script type="text/javascript"> alert("Blog Content is Plagiarized. The content has exceeded the 50% Threshold for plagiarism with a '.$response['percentPlagiarism'].' percent. Please rewrite it!") </script>';
+                    exit();
+                }
+
+            }            
+        }
         
         if (!empty($content))
         {
@@ -203,13 +263,13 @@
                                                     <th>Joined:</th>
                                                     <td>Sep 27, 2018</td>
                                                 </tr>
-                                                <tr>
+                                                <!--<tr>
                                                     <th>Messages:</th>
                                                     <td>3</td>
-                                                </tr>
+                                                </tr> -->
                                                 <tr>
                                                     <th>Likes:</th>
-                                                    <td>0</td>
+                                                    <td> '.$row['post_votes'].' </td>
                                                 </tr>
                                             </table>
                                             <a href="profile.php?id='.$row['idUsers'].'">
