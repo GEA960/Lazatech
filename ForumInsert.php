fe2fb9ebb61c10c9cf714e48c9d3d4455e62398a
Gabby <gabrielalvaro693@gmail.com>
2022-12-07 12:31:08 +0800

    edit forum module will now not accept plagiarized content with >50% plag

--- ForumInsert.php
+++ ForumInsert.php
@@ -6,6 +6,65 @@ if(isset($_POST['update']))
 {
     $PostContent = str_replace("'", "''", $_POST['post_content']);
     $id = $_POST['post_id'];
+
+    $sql = "select * from users where idUsers = 61";
+        $stmt = mysqli_stmt_init($conn);    
+        
+        if (!mysqli_stmt_prepare($stmt, $sql))
+        {
+            die('SQL error');
+        }
+        else
+        {
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            $user = mysqli_fetch_assoc($result);
+
+            $txt = $PostContent;
+            
+            $postData = [ "text" => $txt,
+                        "language" => "en",
+                        "includeCitations" => "false",
+                        "scrapeSources" => "false"
+                        ];
+            $api =  $user['PlagAPIKey'];
+
+            $curl = curl_init();
+            
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com/plagiarism",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => json_encode($postData),
+                CURLOPT_HTTPHEADER => [
+     "X-RapidAPI-Host: plagiarism-checker-and-auto-citation-generator-multi-lingual.p.rapidapi.com",
+     "X-RapidAPI-Key: $api",
+     "content-type: application/json"
+                ],
+            ]);
+            
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            
+            curl_close($curl);
+         
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                $response = json_decode($response, TRUE);
+                
+                if ($response['percentPlagiarism'] >= "50" ) {
+                    
+                    echo '<script type="text/javascript"> alert("Blog Content is Plagiarized. The content has exceeded the 50% Threshold for plagiarism with a '.$response['percentPlagiarism'].' percent. Please rewrite it!") </script>';
+                    exit();
+                }
+
+            }            
     $query = "UPDATE posts SET post_content='$PostContent' WHERE post_id='$_POST[post_id]'";
     $query_run = mysqli_query($conn, $query);                                                                 
     
@@ -16,5 +75,5 @@ if(isset($_POST['update']))
         echo '<script type="text/javascript"> alert("Data Not Updated") </script>';
     }
 }
-
+}
 ?>
