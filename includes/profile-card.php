d005124a4f8d4f26ce08ee7d531a6955f88e194d
Gabriel Edrian A. Alvaro <75560222+GEA960@users.noreply.github.com>
2023-02-27 11:21:44 +0800

    Update profile-card.php

--- includes/profile-card.php
+++ includes/profile-card.php
@@ -25,7 +25,7 @@
                         <small class="text-muted"><?php echo $_SESSION['headline']; ?></small>
                     </h4>
                 </div>
-                 <p><span class="small"> Note: All transactions obtained from Lazatech Educate and Social media are all for testing first. 
+                 <p><span class="small"> Note: All transactions obtained from Lazatech Educate and Social media are all for testing for the meantime. 
                The tokens obtained will not be transferred to XUMM wallet as the testing and development phase are ongoing. 
                Once the testing phase is finished, all balances will be reset to zero. 
                However, the respondents and beta testers will receive 10 LZT tokens from the year 2022 up to 2/24/2023 - 9:00 am, 
